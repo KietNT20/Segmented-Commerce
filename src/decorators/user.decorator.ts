efed6035f4e453fdc3543e7f822729820780b8f0
nguyentuankiet <ntk28042002@gmail.com>
2025-09-28 11:57:50 +0700

    Refactor user and JWT auth decorators to improve type safety in request context

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -1,10 +1,11 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { Request } from 'express';
 
 export const CurrentUser = createParamDecorator(
     (data: unknown, context: ExecutionContext) => {
         const ctx = GqlExecutionContext.create(context);
-        const request = ctx.getContext().req;
+        const request = ctx.getContext<{ req: Request }>().req;
         return request.user;
     },
 );
