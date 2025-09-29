6bc187e2fc1433c9c69d417fe9c40ed77a1e3313
nguyentuankiet <ntk28042002@gmail.com>
2025-09-29 15:24:55 +0700

    Refactor GqlAuthGuard and GqlLocalAuthGuard to improve type safety in getRequest method

--- src/guards/jwt-auth.guard.ts
+++ src/guards/jwt-auth.guard.ts
@@ -1,12 +1,12 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 @Injectable()
 export class GqlAuthGuard extends AuthGuard('jwt') {
-    getRequest(context: ExecutionContext) {
+    getRequest(context: ExecutionContext): Request {
         const ctx = GqlExecutionContext.create(context);
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
-        return ctx.getContext().req;
+        return ctx.getContext<{ req: Request }>().req;
     }
 }
--- src/guards/local-auth.guard.ts
+++ src/guards/local-auth.guard.ts
@@ -1,17 +1,18 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 @Injectable()
 export class GqlLocalAuthGuard extends AuthGuard('local') {
-    getRequest(context: ExecutionContext) {
+    getRequest(context: ExecutionContext): Request {
         const gqlExecutionContext = GqlExecutionContext.create(context);
-        const gqlContext = gqlExecutionContext.getContext();
-        const gqlArgs = gqlExecutionContext.getArgs();
-        gqlContext.req.body = { ...gqlContext.req.body, ...gqlArgs };
+        const gqlContext = gqlExecutionContext.getContext<{ req: Request }>();
+        const gqlArgs: Record<string, unknown> = gqlExecutionContext.getArgs();
+        gqlContext.req.body = {
+            ...(gqlContext.req.body as Record<string, unknown>),
+            ...gqlArgs,
+        };
         return gqlContext.req;
     }
 }
